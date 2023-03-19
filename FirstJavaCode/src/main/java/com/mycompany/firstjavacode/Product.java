9588c693136d64d51a353e949d709ef1569ffb65
Tamim Ahmed <tamimahmed357@gmail.com>
2023-03-19 21:56:19 +0600

    File has been added to the repository for uploading in GitHub

---
+++ FirstJavaCode/src/main/java/com/mycompany/firstjavacode/Product.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.firstjavacode;
+
+public class Product {
+    
+    public static void main(String[] args){
+        
+        System.out.println("ID: 101.");
+        System.out.println("Title: iphone15,");
+        System.out.println("Price: 1895 euros,");
+        System.out.println("Description: Perfect product with best image quality,");
+        System.out.println("Category: Phone");
+    
+    }
+    
+}
